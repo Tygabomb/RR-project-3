f52b7d12cba37e8d8bd61b7df6e3325089ad9257
Your Name <you@example.com>
2019-03-23 14:03:32 -0700

    adding schema and user model

---
+++ client/models/users.js
@@ -0,0 +1,42 @@
+module.exports = function (sequelize, DataTypes) {
+  const users = sequelize.define("users", {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    firstName: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    lastName: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    userName: {
+      type: DataTypes.STRING,
+      defaultValue: "UserName",
+      allowNull: false
+    },
+    userPassword: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    userEmail: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    userLat: {
+        type: DataTypes.DECIMAL(10,8),
+        allowNull: true,
+    },
+    userLng: {
+        type: DataTypes.DECIMAL(11, 8),
+        allowNull: true
+    }
+  }, {
+      freezeTableName: true,
+
+    });
+  return users;
+};
