b9ad4942db3f4ff8978e1594856f5ce6b858fef8
Your Name <you@example.com>
2019-03-25 18:47:41 -0700

    adding date to user  model

--- client/models/users.js
+++ client/models/users.js
@@ -26,6 +26,11 @@ module.exports = function (sequelize, DataTypes) {
       type: DataTypes.STRING,
       allowNull: false
     },
+    date: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.DATE.CURRENT_TIMESTAMP
+    },
     userLat: {
         type: DataTypes.DECIMAL(10,8),
         allowNull: true,
