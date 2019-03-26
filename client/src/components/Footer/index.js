96755941ae70d6ad193963d0ca48cb2c67fb6105
bking1989 <bradley_king@me.com>
2019-03-25 19:09:46 -0700

    Finished footer links and added logo

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -1,37 +1,47 @@
-import React from "react";
+import React, { Component } from "react";
 
 const footerStyle = {
     backgroundColor: "rgb(0,123,255)",
     minHeight: "60px"
 }
 
-function Footer(props) {
-    return (
-        <footer className="d-block fixed-bottom w-100" style={footerStyle}>
-            <div className="container">
-                <div className="row">
-                    <div className="col-4 col-sm-6">
-                        <img src="./images/Yelp_trademark_RGB.png" />
-                    </div>
-                    <div className="col-4 col-sm-3 ml-auto">
-                        <ul className="list-group list-group-flush my-3">
-                            <li className="list-group-item">Ashley</li>
-                            <li className="list-group-item">Bradley</li>
-                            <li className="list-group-item">Brandon</li>
-                            <li className="list-group-item">Josh</li>
-                        </ul>
-                    </div>
-                    <div className="col-4 col-sm-3 ml-auto">
-                        <ul className="list-group list-group-flush my-3">
-                            <li className="list-group-item">Home</li>
-                            <li className="list-group-item">Reviews</li>
-                            <li className="list-group-item">Contact</li>
-                        </ul>
+class Footer extends Component {
+    super(props) {
+    }
+
+    imageStyle = {
+        maxWidth: "120px"
+    }
+
+    render() {
+        return (
+            <footer className="d-block fixed-bottom w-100" style={footerStyle}>
+                <div className="container">
+                    <div className="row">
+                        <div className="col-4 col-sm-6">
+                            <p className="text-white d-inline-block mt-3 mt-sm-0">Powered by</p>
+                            <img alt="" className="img-fluid d-inline-block" src={"/images/Yelp_trademark_RGB.png"} style={this.imageStyle} />
+                        </div>
+                        <div className="col-4 col-sm-3 ml-auto">
+                            <ul className="list-group list-group-flush my-3">
+                                <li className="list-group-item"><a className="text-dark" href="/">Ashley</a></li>
+                                <li className="list-group-item"><a className="text-dark" href="/">Bradley</a></li>
+                                <li className="list-group-item"><a className="text-dark" href="/">Brandon</a></li>
+                                <li className="list-group-item"><a className="text-dark" href="/">Josh</a></li>
+                            </ul>
+                        </div>
+                        <div className="col-4 col-sm-3 ml-auto">
+                            <ul className="list-group list-group-flush my-3">
+                                <li className="list-group-item"><a className="text-dark" href="/">Home</a></li>
+                                <li className="list-group-item"><a className="text-dark" href="/">Reviews</a></li>
+                                <li className="list-group-item"><a className="text-dark" href="/">Contact</a></li>
+                            </ul>
+                        </div>
                     </div>
                 </div>
-            </div>
-        </footer>
-    );
+            </footer>
+        )
+    };
 }
 
 export default Footer;
