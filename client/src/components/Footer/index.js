eb44e1af3f40762d032d1acebdc8e7dce57b5046
bking1989 <bradley_king@me.com>
2019-03-25 18:41:50 -0700

    Starting to finish up on footer

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -10,20 +10,22 @@ function Footer(props) {
         <footer className="d-block fixed-bottom w-100" style={footerStyle}>
             <div className="container">
                 <div className="row">
-                    <div className="col-6"></div>
-                    <div className="col-3 ml-auto">
-                        <ul className="list-group my-3">
-                            <li className="list-group-item text-center text-sm-left">Ashley</li>
-                            <li className="list-group-item text-center text-sm-left">Bradley</li>
-                            <li className="list-group-item text-center text-sm-left">Brandon</li>
-                            <li className="list-group-item text-center text-sm-left">Josh</li>
+                    <div className="col-4 col-sm-6">
+                        <img src="./images/Yelp_trademark_RGB.png" />
+                    </div>
+                    <div className="col-4 col-sm-3 ml-auto">
+                        <ul className="list-group list-group-flush my-3">
+                            <li className="list-group-item">Ashley</li>
+                            <li className="list-group-item">Bradley</li>
+                            <li className="list-group-item">Brandon</li>
+                            <li className="list-group-item">Josh</li>
                         </ul>
                     </div>
-                    <div className="col-3 ml-auto">
-                        <ul className="list-group my-3">
-                            <li className="list-group-item text-center text-sm-left">Home</li>
-                            <li className="list-group-item text-center text-sm-left">Reviews</li>
-                            <li className="list-group-item text-center text-sm-left">Contact</li>
+                    <div className="col-4 col-sm-3 ml-auto">
+                        <ul className="list-group list-group-flush my-3">
+                            <li className="list-group-item">Home</li>
+                            <li className="list-group-item">Reviews</li>
+                            <li className="list-group-item">Contact</li>
                         </ul>
                     </div>
                 </div>
--- server.js
+++ server.js
@@ -19,6 +19,7 @@ if (process.env.NODE_ENV === "production") {
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
+
 app.listen(port, function(error) {
   if (error) {
     console.error(error)
