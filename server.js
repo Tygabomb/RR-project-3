628921d4841a85d5c24c2000c2ab8bc14d43d97e
Brandon Harrell <BHarrell@myrgroup.com>
2019-03-30 08:03:55 -0700

    env file update

--- server.js
+++ server.js
@@ -12,16 +12,40 @@ if (process.env.NODE_ENV === "production") {
 }
 
 // Define API routes here
+const mysql = require('mysql');
 
+let conn;
+
+if (process.env.JAWSDB_URL) {
+    conn = mysql.createConnection(process.env.JAWSDB_URL)
+} else {
+    mysql.createConnection({
+        host: DB_HOST,
+        port: 3306,
+        user: DB_USER,
+        password: DB_PASSWORD,
+        database: DB_NAME,
+    });
+}
+
+conn.connect((err) => {
+    if (err) {
+        console.error(`Error connecting: ${err.stack}`);
+        return;
+    }
+    console.log(`connected as id: ${conn.threadId}`);
+});
+
+module.exports = conn;
 // Send every other request to the React app
 // Define any API routes before this runs
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
-app.listen(port, function (error) {
+app.listen(PORT, (error) => {
   if (error) {
     console.error(error)
   } else {
-    console.info("==> 🌎  Listening on port %s. Open up http://localhost:%s/ in your browser.", port, port)
+    console.info(`==> 🌎 Listening on port %s. Open up http://localhost:%s/ in your browser. ${PORT}`)
   }
 })
