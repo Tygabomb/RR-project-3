cfc951db7af9a35d271257b98b3c66fb30b3c07b
Brandon Harrell <BHarrell@myrgroup.com>
2019-03-31 22:48:31 -0700

    express update 002

--- server.js
+++ server.js
@@ -15,7 +15,17 @@ if (process.env.NODE_ENV === "production") {
 }
 
 // Use apiRoutes
-app.use("/api", apiRoutes);
+// app.use("/api", apiRoutes);
+
+app.get('/api/users', (req, res) => {
+  const users = [
+    {id: 1, firstName: 'John', lastName: 'Doe'},
+    {id: 2, firstName: 'Brad', lastName: 'Traversy'},
+    {id: 3, firstName: 'Mary', lastName: 'Swanson'},
+  ];
+
+  res.json(users);
+});
 
 db.sequelize.sync( {/*force: true*/}).then(function() {
   app.listen(PORT, ()=>{
