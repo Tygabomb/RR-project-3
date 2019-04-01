2850cc5e6bbc31dd18bc0bf291b913272424e986
Brandon Harrell <BHarrell@myrgroup.com>
2019-03-31 21:32:50 -0700

    express additions 001

--- client/server/routes/api/index.js
+++ client/server/routes/api/index.js
@@ -8,4 +8,21 @@ const routes = require("./v1");
 // API Routes
 router.use("/v1", routes);
 
+
+
+import axios from "axios";
+
+// Export an object containing methods we'll use for accessing the Yelp API
+
+export default {
+  getRandomResturant: function() {
+    return axios.get("https:// ");
+  },
+  
+  getResturantList: function() {
+    return axios.get("https:// ");
+  }
+};
+
+
 module.exports = router;
