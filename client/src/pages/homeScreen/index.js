6f2aab5e9fc926d55cf6964dd33dd88d8deb79c8
Josh Romea <josh.romea@gmail.com>
2019-04-13 09:59:10 -0700

    data appears in state

--- client/src/pages/homeScreen/index.js
+++ client/src/pages/homeScreen/index.js
@@ -4,22 +4,21 @@ import API from "../../utils/api";
 
 
 
-
-
 class HomeScreen extends Component {
     state = {
         options: [],
         baseSize: 300,
         apiData: [],
         chosenName: '',
-        chosenUrl: '',
+        chosenYelpUrl: '',
         chosenRating: 0,
-        chosenLocation: '',
-        chosenCoordinates: {
-            latitude: 0,
-            longitude: 0
-        },
-        chosenImage_url: ''
+        chosenAddress: {},
+        chosenCoordinates: {},
+        chosenImage_url: '',
+        chosenCategories: [],
+        chosenPhoneNum: '',
+        chosenId: '',
+        chosenPrice: ''
     }
     componentDidMount() {
         this.setState({
@@ -31,22 +30,35 @@ class HomeScreen extends Component {
         this.setState({ chosenName: name })
     }
 
-    handleOnComplete = value => {
-        return value
+    handleOnComplete = () => {
+        for (let i = 0; i < this.state.apiData.length; i++) {
+            const { restName, restId, restImageUrl, restRating, restCoordinates, restPrice, restLocation, restCategories, restPhoneNum, yelpUrl } = this.state.apiData[i]
+            const address = restLocation.display_address
+            if (restName === this.state.chosenName) {
+                this.setState({
+                    chosenId: restId,
+                    chosenImage_url: restImageUrl,
+                    chosenRating: restRating,
+                    chosenCoordinates: restCoordinates,
+                    chosenPrice: restPrice,
+                    chosenAddress: address,
+                    chosenCategories: restCategories,
+                    chosenPhoneNum: restPhoneNum,
+                    chosenYelpUrl: yelpUrl
+                })
+            }
+        }
     }
 
     getRestaurantData = (cb) => {
-
         API.getRandomResturant()
             .then(res => {
                 let apiDataArr = []
                 let nameArr = []
                 res.data.forEach(business => {
-                    let { name, id, image_url, rating, coordinates, price, location } = business
-                    // console.log(name)
+                    const { name, id, image_url, rating, coordinates, price, location, categories, display_phone, url } = business
                     nameArr.push(name)
-                    apiDataArr.push({ restName: name, restId: id, restImageUrl: image_url, restRating: rating, restCoordinates: coordinates, restPrice: price, restLocation: location})
-                    // console.log(nameArr)  
+                    apiDataArr.push({ restName: name, restId: id, restImageUrl: image_url, restRating: rating, restCoordinates: coordinates, restPrice: price, restLocation: location, restCategories: categories, restPhoneNum: display_phone, yelpUrl: url })
                     this.setState({
                         options: nameArr,
                         apiData: apiDataArr
@@ -54,7 +66,6 @@ class HomeScreen extends Component {
                         cb()
                     })
                 })
-                console.log(this.state.apiData)
             })
             .catch(err => console.log(err))
     }
@@ -74,12 +85,6 @@ class HomeScreen extends Component {
             return "Nothing is here"
         }
     }
-
-
-    // callApi = () => {
-    //     this.setState({options: ["wingo", "oingo", "boingo"]})
-    // }
-
 
     render() {
         return (
