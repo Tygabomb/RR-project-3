01eb8501d12393d366fbb0fc61debb36c4be96f1
Josh Romea <josh.romea@gmail.com>
2019-04-15 14:54:39 -0700

    map frame renders but no map

--- client/src/components/Maps/index.js
+++ client/src/components/Maps/index.js
@@ -1,10 +1,32 @@
-import React from 'react';
-import './style';
+import React, { Component } from 'react';
+import { Map, GoogleApiWrapper } from 'google-maps-react';
+import Keys from '../../keys'
+import './style.css'
 
-function Maps(){
-    return (
-        <div className="map" id="map">
+class Maps extends Component {
 
-        </div>
-    )
+    mapStyles = {
+        width: '300px',
+        height: '300px'
+    };
+
+    render() {
+        return (
+            <div className="map" id="map">
+                <Map
+                    google={this.props.google}
+                    zoom={14}
+                    initialCenter={{
+                        lat: this.props.chosenLat,
+                        lng: this.props.chosenLong
+                    }}
+                    style={this.mapStyles}
+                />
+            </div>
+        )
+    }
 }
+
+export default GoogleApiWrapper({
+    apiKey: `AIzaSyA2D14kzQZWyRlsGpnax8Gi_iM603ssnnU`
+})(Maps)
