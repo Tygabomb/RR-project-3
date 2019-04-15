a316f006dfa1f5fe051afdc0c4059f3e447f5ecb
Josh Romea <josh.romea@gmail.com>
2019-04-15 11:27:16 -0700

    stash

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -20,6 +20,10 @@ class Footer extends Component {
         maxWidth: "120px"
     };
 
+    roundedCorners = {
+        overflow: "hidden"
+    };
+
     render() {
         return (
             <footer className="d-block w-100" style={footerStyle}>
@@ -29,19 +33,12 @@ class Footer extends Component {
                             <p className="text-white d-inline-block mt-3 mt-sm-0" style={shadowStyle}>Powered by</p>
                             <img alt="" className="img-fluid d-inline-block" src={"/images/Yelp_trademark_RGB.png"} style={this.imageStyle} />
                         </div>
-                        <div className="col-4 col-sm-3 ml-auto">
-                            <ul className="list-group my-3">
-                                <li className="list-group-item"><a className="text-dark" href="https://github.com/Amal2774" rel="noopener noreferrer" target="_blank">Ashley</a></li>
-                                <li className="list-group-item"><a className="text-dark" href="https://github.com/bking1989" rel="noopener noreferrer" target="_blank">Bradley</a></li>
-                                <li className="list-group-item"><a className="text-dark" href="https://github.com/Tygabomb" rel="noopener noreferrer" target="_blank">Brandon</a></li>
-                                <li className="list-group-item"><a className="text-dark" href="https://github.com/joshromea" rel="noopener noreferrer" target="_blank">Josh</a></li>
-                            </ul>
-                        </div>
-                        <div className="col-4 col-sm-3 ml-auto">
-                            <ul className="list-group my-3">
-                                <li className="list-group-item"><Link className="text-dark" to="/app">Home</Link></li>
-                                <li className="list-group-item"><Link className="text-dark" to="/profile">My Profile</Link></li>
-                                <li className="list-group-item"><Link className="text-dark" to="/">Log Out</Link></li>
+                        <div className="col col-sm-6 ml-auto">
+                            <ul className="list-inline my-3 float-right rounded" style={this.roundedCorners}>
+                                <li className="list-inline-item bg-white py-2 px-3 m-0 border-right border-dark"><a className="text-dark" href="https://github.com/Amal2774" rel="noopener noreferrer" target="_blank">Ashley</a></li>
+                                <li className="list-inline-item bg-white py-2 px-3 m-0 border-right border-dark"><a className="text-dark" href="https://github.com/bking1989" rel="noopener noreferrer" target="_blank">Bradley</a></li>
+                                <li className="list-inline-item bg-white py-2 px-3 m-0 border-right border-dark"><a className="text-dark" href="https://github.com/Tygabomb" rel="noopener noreferrer" target="_blank">Brandon</a></li>
+                                <li className="list-inline-item bg-white py-2 px-3 m-0"><a className="text-dark" href="https://github.com/joshromea" rel="noopener noreferrer" target="_blank">Josh</a></li>
                             </ul>
                         </div>
                     </div>
