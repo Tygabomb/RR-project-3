ab61ea88f3834de41d62979ed664ce7bfe49060f
Josh Romea <josh.romea@gmail.com>
2019-04-15 17:27:44 -0700

    got rid of force sync on server

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.use("/api", favoriteRoutes)
 
 app.use(userRoutes);
 
-db.sequelize.sync({ force: true }).then(function () {
+db.sequelize.sync().then(function () {
   app.listen(PORT, () => {
     console.log(`🌎 ==> API server now on port ${PORT}!`);
   })
